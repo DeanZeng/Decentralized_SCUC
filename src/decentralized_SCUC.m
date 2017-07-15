290f7c1b96f10f5faac4ba3b289709893a967b5f
DeanZeng <zfangdi@gmail.com>
2017-07-15 20:24:57 +0800

    complete framework， need debuging

--- src/decentralized_SCUC.m
+++ src/decentralized_SCUC.m
@@ -11,19 +11,20 @@ ftie_outC= cell(A,1);
 ftie_avgC= cell(A,1);
 
 QUIET    = false;
-MAX_ITER = 4;
+MAX_ITER = 100;
 epsP     = 1;
 epsD     = 1e-1;
 Rho      = 0.001;
 alpha    = 1;
-quiet    = false(A,1);
-parpool(A);
+% parpool(A);
 tic;
 spmd
         % load data in work;
         scuc_in = load(matFile{labindex});
         % variables for ADMM
-        lamda = zeros(scuc_in.T,scuc_in.Ntie);
+        ftie_out = zeros(scuc_in.T,scuc_in.Ntie);           % x
+        ftie_avg = zeros(scuc_in.T,scuc_in.Ntie);           % z
+        lamda = zeros(scuc_in.T,scuc_in.Ntie);              % y
         resP  = 10*ones(scuc_in.T,scuc_in.Ntie);
         resD  = 10*ones(scuc_in.T,scuc_in.Ntie);
         % tie data in client; 
@@ -32,10 +33,7 @@ spmd
         TieBus  = scuc_in.TieBus;
         Tie_Bus  = scuc_in.Tie_Bus;
         % scuc model define in local work;
-        scuc_model = scuc_modelDefine(scuc_in);
-        ftie_avg = zeros(scuc_in.T,scuc_in.Ntie);
-        %%--------------------------- x update --------------------------------
-        [scuc_out,ftie_out] = scuc_modelSolve(scuc_model,ftie_avg,lamda,0);   
+        scuc_model = scuc_modelDefine(scuc_in); 
 end
 for a=1:A
     NtieC{a}     = Ntie{a};
@@ -45,8 +43,20 @@ for a=1:A
     ftie_outC{a} = ftie_out{a};
     ftie_avgC{a} = ftie_avg{a};
 end
-for k= 2:MAX_ITER
+for k= 1:MAX_ITER
+    if k==1
+        spmd
+            %%--------------------------- x update -------------------------------
+            [scuc_out,ftie_out] = scuc_modelSolve(scuc_model,ftie_avg,lamda,0);   
+        end
+    else 
+        spmd
+            %%--------------------------- x update -------------------------------
+            [scuc_out,ftie_out] = scuc_modelSolve(scuc_model,ftie_avg,lamda,Rho);   
+        end
+    end
     %%--------------------------- z update --------------------------------
+    % fetch x
     for a=1:A
         ftie_outC{a} = ftie_out{a};
     end
@@ -56,16 +66,19 @@ for k= 2:MAX_ITER
             ab = Tie_BusC{a};
             bb = TieBusC{a}(la);
             lb = find ( (TieAreaC{b}==a)&(TieBusC{b}==ab)&(Tie_BusC{b}== bb));
-            %% communication
-            ftie_avgC{a}(:,la,k) = 0.5*(ftie_outC{a}(:,la)-ftie_outC{b}(:,lb));
+            % update z
+            ftie_avgC{a}(:,la) = 0.5*(ftie_outC{a}(:,la)-ftie_outC{b}(:,lb));
         end
     end
     spmd
-            ftie_avg = ftie_avgC{labindex};
-            lamda = lamda + Rho*(ftie_out - ftie_avg(:,:,k));
-            resP  = (ftie_out - ftie_avg(:,:,k));
-            resD  = Rho*(ftie_avg(:,:,k)-ftie_avg(:,:,k-1));   
-            quiet = (all(all(resP <= epsP)))&&(all(all(resD <= epsD)));
+        ftie_avg_old = ftie_avg;
+        % fetch z
+        ftie_avg = ftie_avgC{labindex};
+        % update y
+        lamda = lamda + Rho*(ftie_out - ftie_avg);
+        resP  = (ftie_out - ftie_avg);
+        resD  = Rho*(ftie_avg-ftie_avg_old);   
+        quiet = (all(all(resP <= epsP)))&&(all(all(resD <= epsD)));
     end
     QUIET = true;
     for a=1:A
@@ -74,11 +87,7 @@ for k= 2:MAX_ITER
     if QUIET
         break;
     end
-    spmd
-        %%--------------------------- x update -------------------------------
-        [scuc_out,ftie_out]   = scuc_modelSolve(scuc_model,ftie_avg(:,:,k),lamda,Rho);   
-    end
     k
     toc;
 end
-delete(gcp());
+% delete(gcp());
