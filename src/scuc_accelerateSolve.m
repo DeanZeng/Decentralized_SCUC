1af1597318a260d95e27b865a32fae68216c8a9d
DeanZeng <zfangdi@gmail.com>
2017-08-17 20:53:25 +0800

    correct mistakes： the input parameters of function are formal parameter

--- src/scuc_accelerateSolve.m
+++ src/scuc_accelerateSolve.m
@@ -1,15 +1,19 @@
-function [out,ftie_out,iu] = scuc_accelerateSolve(model,ftie_avg,lamda,Rho,iu)
+function [out,ftie_out,iu] = scuc_accelerateSolve(model,ftie_avg,lamda,Rho,iu,MIPGap)
+if nargin <= 5
+    MIPGap = 0.005;
+end
 %% insensitive unit struct
 % % iu.onoff_old   TxNg    % 机组上次迭代状态 
 % % iu.invar_times 1xNg;   % 机组状态不变次数
 % % iu.fix_units   1xNg;   % 状态固定机组 0-没有固定， 1-固定，未加入约束， 2-固定，已加入约束 
 % % iu.insens_times;       % 机组状态变化不灵敏判断次数 >=2
+Constraints = model.Constraints;
 [T,Ng] = size(iu.onoff_old);
 %% fix unit status
 for g =1:Ng
     if iu.fix_units(g)==1
-        model.Constraints = [model.Constraints, (model.Variable.onoff(:,g) == iu.onoff_old(:,g)):'fix unit status'];
-        iu.fix_units(g)=2;
+        Constraints = [Constraints, (model.Variable.onoff(:,g) == iu.onoff_old(:,g)):'fix unit status'];
+%         iu.fix_units(g)=2;
     end
 end
 %% objective
@@ -18,12 +22,12 @@ Lagrangian = model.Objective +...
     sum(sum(Rho*0.5*(model.Variable.ftie-ftie_avg).*(model.Variable.ftie-ftie_avg)));
 %% solve
 Ops = sdpsettings('solver','gurobi','usex0',1,'verbose',0,'showprogress',0);
-Ops.gurobi.MIPGap=0.005;
+Ops.gurobi.MIPGap=MIPGap;
 %         Ops.gurobi.MIPGapAbs=1.0;
 Ops.gurobi.OptimalityTol = 0.0002;
 %         Ops.gurobi.FeasRelaxBigM   = 1.0e10;
 Ops.gurobi.DisplayInterval = 20;
-diagnose = optimize(model.Constraints,Lagrangian,Ops); 
+diagnose = optimize(Constraints,Lagrangian,Ops); 
 % check(Constraints);
 if diagnose.problem ~= 0
     error(yalmiperror(diagnose.problem));
